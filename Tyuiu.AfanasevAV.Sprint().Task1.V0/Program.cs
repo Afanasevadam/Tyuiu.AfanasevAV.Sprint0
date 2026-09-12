3ee9f8d3327a88fe22b23fa21cc3883ca90337e6
Tyuiu.AfanasevAV@outlook.com <Tyuiu.AfanasevAV@outlook.com>
2026-09-12 21:44:56 +0500

    Task1

---
+++ Tyuiu.AfanasevAV.Sprint().Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SpirinIS.Sprint0.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Адам";
+            string age = "19";
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+        
+        }
+}
