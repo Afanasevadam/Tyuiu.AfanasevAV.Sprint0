f6040c8bd656c3d84c070f72847b3ca31ccb6103
Tyuiu.AfanasevAV@outlook.com <Tyuiu.AfanasevAV@outlook.com>
2026-09-13 22:49:52 +0500

    Task4

---
+++ Tyuiu.AfanasevAV.Sprint0.Task4.V1.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AfanasevAV.Sprint0.Task4.V1.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
