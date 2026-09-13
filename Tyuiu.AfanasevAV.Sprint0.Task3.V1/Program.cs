e47a3b63c746d16be6b515bd26350b6f0c40f0e4
Tyuiu.AfanasevAV@outlook.com <Tyuiu.AfanasevAV@outlook.com>
2026-09-13 22:29:19 +0500

    Task3

---
+++ Tyuiu.AfanasevAV.Sprint0.Task3.V1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.AfanasevAV.Sprint0.Task3.V1.Lib;
+
+namespace Tyuiu.AfanasevAV.Sprint0.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
